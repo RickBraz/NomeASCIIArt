c3740f706758689783d3bbb9ab242e4cc5821b7a
= <=>
2020-06-05 17:33:33 -0300

    adicionei o programa

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Figgle;
+using Humanizer;
+
+namespace NomeASCIIArt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Figgle: criando textos em ASCII art");
+            Console.WriteLine("Digite seu nome");
+            string SeuNome  = FiggleFonts.Standard.Render(Console.ReadLine());
+            Console.WriteLine(SeuNome);
+        }
+    }
+}
